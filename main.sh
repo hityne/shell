52adf10f029489e6b53fae1f5b1c5b5d21e88e23
hityne <long@live.ca>
2022-04-17 07:47:57 +0800

    ..

--- main.sh
+++ main.sh
@@ -118,7 +118,7 @@ elif [ "$main_no" = "5" ]; then
 	#Break here
 	read -n 1 -p "Press any key to continue..."
 	old_port=$(sed -n '/^Port/'p /etc/ssh/sshd_config)
-	sed -i 's/'$old_port'/Port '$ssh_port'/g' /etc/ssh/sshd_config
+	sed -i "s/$old_port/Port $ssh_port/g" /etc/ssh/sshd_config
 	systemctl restart ssh
 	echo""
 	echo "Service sshd has been restarted. Please use the new SSH port to login."
