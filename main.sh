f267b458725981e826a646cadbb60e9d2b550677
hityne <accdou@163.com>
2022-06-27 20:55:17 +0800

    ..

--- main.sh
+++ main.sh
@@ -167,9 +167,9 @@ elif [ "$main_no" = "9" ]; then
 	service ntp stop                 
     ntpdate us.pool.ntp.org          
 	service ntp start
-	date -R              
-				
+	date -R              		
 	fi
+	
 elif [ "$main_no" = "10" ]; then
 	wget https://github.com/hityne/ssh/raw/master/dmytest.sh && chmod +x dmytest.sh && bash dmytest.sh && rm -f dmytest.sh
 	
